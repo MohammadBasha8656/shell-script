659f9a921e1919bd39d6b24f4dd5b23e4e6cb666
harshad <mohammadbasha8656@gmail.com>
2026-01-30 12:30:42 +0530

    HARSHA BIO

---
+++ 01-hello.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh. I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. What are you learning ramesh?"
+echo "Ramesh:: I am learning Shell. What about you?"
